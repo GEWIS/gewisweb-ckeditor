36c3b3a28798f9bd6129ba0c60ff4f336f7b03c9
Tom Udding <tomudding@users.noreply.github.com>
2022-08-17 19:35:58 +0200

    Initial release of the GEWISWEB CKEditor

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+'use strict';
+
+/* eslint-env node */
+
+const path = require('path');
+const webpack = require('webpack');
+const { bundler, styles } = require('@ckeditor/ckeditor5-dev-utils');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'ckeditor.js'),
+
+    output: {
+        library: 'ClassicEditor',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'ckeditor.js',
+        libraryTarget: 'umd',
+        libraryExport: 'default'
+    },
+
+    plugins: [
+        new CKEditorWebpackPlugin({
+            language: 'en',
+            additionalLanguages: ['nl']
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }),
+        new webpack.BannerPlugin({
+            banner: bundler.getLicenseBanner(),
+            raw: true
+        })
+    ],
+
+    optimization: {
+		minimizer: [
+			new TerserPlugin( {
+				terserOptions: {
+					output: {
+						// Preserve CKEditor 5 license comments.
+						comments: /^!/
+					}
+				},
+				extractComments: false
+			} )
+		]
+	},
+
+    module: {
+        rules: [
+            {
+                test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+                use: [ 'raw-loader' ]
+            },
+            {
+                test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: styles.getPostCssConfig( {
+                                themeImporter: {
+                                    themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+                                },
+                                minify: true
+                            } )
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+
+    // Useful for debugging.
+    devtool: 'source-map',
+
+    // By default webpack logs warnings if the bundle is bigger than 200kb.
+    performance: { hints: false }
+};
